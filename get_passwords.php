d93e2d5fbd8b53b5887987b2d986ea9dd06aed94
Tiwonge Thawe <thawetiwonge3@gmail.com>
2025-02-19 12:34:34 +0200

    gp2

--- get_passwords.php
+++ get_passwords.php
@@ -2,7 +2,7 @@
 require_once "config.php"; // Ensure database connection is established
 
 try {
-    $query = "SELECT id, website, username FROM passwords";
+    $query = "SELECT id, website, username, password FROM passwords";
     $stmt = $conn->prepare($query);
     $stmt->execute();
 
